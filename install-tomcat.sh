6c87364dd983c2dda24625cfbd105f7a028135b3
sangamesh288 <sangameshgbali1995@gmail.com>
2025-03-14 14:19:12 +0530

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -7,9 +7,9 @@ sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.102/bin/apache-tomcat-9.
 sudo tar xvzf apache-tomcat-9.*
 sudo rm -rf apache*.gz
 sudo git clone https://github.com/sangamesh288/tomcat_conf.git
-sudo cp /tomcat_conf/context.xml /opt/apache-tomcat-9.0.102/webapps/manager/META-INF/context.xml
-sudo cp /tomcat_conf/context.xml /opt/apache-tomcat-9.0.102/webapps/host-manager/META-INF/context.xml
-sudo cp /tomcat_conf/tomcat-users.xml /opt/apache-tomcat-9.0.102/conf/tomcat-users.xml
+sudo cp tomcat_conf/context.xml /opt/apache-tomcat-9.0.102/webapps/manager/META-INF/context.xml
+sudo cp tomcat_conf/context.xml /opt/apache-tomcat-9.0.102/webapps/host-manager/META-INF/context.xml
+sudo cp tomcat_conf/tomcat-users.xml /opt/apache-tomcat-9.0.102/conf/tomcat-users.xml
 sudo rm -rf tomcat_conf
 cd /opt/apache-tomcat-9.*
 sudo sh bin/startup.sh
