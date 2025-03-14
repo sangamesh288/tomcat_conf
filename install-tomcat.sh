86a4f232032a550c38a6f2365f356a388422805c
sangamesh288 <sangameshgbali1995@gmail.com>
2025-03-14 13:07:16 +0530

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -2,10 +2,8 @@
 ver=`curl --silent http://mirror.vorboss.net/apache/tomcat/tomcat-9/ | grep v9 | awk '{split($5,c,">v") ; split(c[2],d,"/") ; print d[1]}'`
 sudo yum install -y java wget git
 cd /opt
-rm -rf apache*
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.102/bin/apache-tomcat-9.0.102.tar.gz
 sudo tar xvzf apache-tomcat-9.*
-sudo rm -rf apache*.gz
 sudo git clone https://github.com/sangamesh288/tomcat_conf.git
 sudo cp ./tomcat_conf/context.xml /opt/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
 sudo cp ./tomcat_conf/context.xml /opt/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
